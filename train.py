c510b9a286362a582e58b191ebd8cec0e9a7392d
Quyền Đặng <ninicom206@gmail.com>
2025-06-20 16:24:02 +0700

    add custom bbox_loss (SIoU) refactor train.py: update training data path to use VOC.yaml for consistency

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ model = YOLO(r'D:\ultralytics_v2\Model_yaml\yolov11n_custom.yaml')
 
 
 # Huấn luyện mô hình với cấu hình dữ liệu
-model.train(data=r'D:\fish-disease-object-detect.v1i.yolov8\data.yaml',
+model.train(data=r'VOC.yaml',
             epochs=1,  # Số lượng epoch
             imgsz=640, # Kích thước ảnh đầu vào
             save_period=5,
